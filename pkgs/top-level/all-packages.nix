c03137fec4bab502586f8aab173e0ec96bcfa48f
Rafael Ieda <git@ieda.me>
2025-10-06 01:21:17 -0300

    cutechess: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13483,8 +13483,6 @@ with pkgs;
   curseofwar = callPackage ../games/curseofwar { SDL = null; };
   curseofwar-sdl = callPackage ../games/curseofwar { ncurses = null; };
 
-  cutechess = qt5.callPackage ../games/cutechess { };
-
   cutemaze = qt6Packages.callPackage ../games/cutemaze { };
 
   ddnet-server = ddnet.override { buildClient = false; };
