35f8b9ed2cdd44896c0d96b5e0202f7f2fbccce6
Rafael Ieda <git@ieda.me>
2026-09-22 22:03:07 -0300

    filezilla: add darwin support

--- pkgs/by-name/fi/filezilla/package.nix
+++ pkgs/by-name/fi/filezilla/package.nix
@@ -8,6 +8,7 @@
   gettext,
   gnutls,
   libfilezilla,
+  libiconvReal,
   libidn,
   nettle,
   pkg-config,
@@ -26,7 +27,7 @@ stdenv.mkDerivation (finalAttrs: {
   version = "3.71.1";
 
   src = fetchurl {
-    # Upstream download link was made unstable on purpose
+    # Upstream download link was made unstable on purpose.
     # See https://trac.filezilla-project.org/ticket/13186
     url = "https://sources.archlinux.org/other/filezilla/filezilla-${finalAttrs.version}.tar.xz";
     hash = "sha256-Pfm5s+Wyw33G2ey4g1VKuhQKAuxHw9a1IzO/FzOvTaQ=";
@@ -36,38 +37,83 @@ stdenv.mkDerivation (finalAttrs: {
     "--disable-manualupdatecheck"
     "--disable-autoupdatecheck"
     "--with-wx-prefix=${wxwidgets_3_2}"
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isDarwin [
+    "--with-pugixml=builtin"
   ];
 
   nativeBuildInputs = [
     autoreconfHook
     pkg-config
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isLinux [
     wrapGAppsHook3
     xdg-utils
   ];
 
   buildInputs = [
     boost
-    dbus
     fzssh
     gettext
     gnutls
     libfilezilla
     libidn
     nettle
-    pugixml
     sqlite
     tinyxml
+    wxwidgets_3_2
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isLinux [
+    dbus
     gtk3
+    pugixml
   ];
 
   strictDeps = true;
-
   enableParallelBuilding = true;
 
-  preFixup = ''
+  preFixup = lib.optionalString stdenv.hostPlatform.isLinux ''
     gappsWrapperArgs+=(
       --suffix PATH : "${lib.makeBinPath [ xdg-utils ]}"
     )
+  '';
+
+  postInstall = lib.optionalString stdenv.hostPlatform.isDarwin ''
+    mkdir -p "$out/Applications"
+    cp -R FileZilla.app "$out/Applications/"
+
+    framework="$out/Applications/FileZilla.app/Contents/Frameworks"
+
+    # FileZilla's macOS bundle contains Apple's libiconv, while
+    # libidn2 requires GNU libiconv.
+    mv "$framework/libiconv.2.dylib" \
+      "$framework/libiconv-apple.2.dylib"
+
+    install_name_tool \
+      -id "@loader_path/libiconv-apple.2.dylib" \
+      "$framework/libiconv-apple.2.dylib"
+
+    # Bundle GNU libiconv for libidn2.
+    cp "${libiconvReal}/lib/libiconv.2.dylib" \
+      "$framework/libiconv.2.dylib"
+
+    install_name_tool \
+      -id "@loader_path/libiconv.2.dylib" \
+      "$framework/libiconv.2.dylib"
+
+    # These libraries use Apple's libiconv ABI.
+    for dylib in \
+      "$framework"/libfilezilla*.dylib \
+      "$framework"/libwx*.dylib \
+      "$framework"/libintl*.dylib
+    do
+      if [ -f "$dylib" ]; then
+        install_name_tool \
+          -change "@loader_path/libiconv.2.dylib" \
+          "@loader_path/libiconv-apple.2.dylib" \
+          "$dylib"
+      fi
+    done
   '';
 
   meta = {
@@ -76,11 +122,11 @@ stdenv.mkDerivation (finalAttrs: {
     longDescription = ''
       FileZilla Client is a free, open source FTP client. It supports
       FTP, SFTP, and FTPS (FTP over SSL/TLS). The client is available
-      under many platforms, binaries for Windows, Linux and macOS are
-      provided.
+      under many platforms; binaries for Windows, Linux and macOS
+      are provided.
     '';
     license = lib.licenses.gpl2;
-    platforms = lib.platforms.linux;
+    platforms = lib.platforms.linux ++ lib.platforms.darwin;
     maintainers = with lib.maintainers; [
       iedame
       pSub
--- pkgs/by-name/li/libfilezilla/package.nix
+++ pkgs/by-name/li/libfilezilla/package.nix
@@ -48,6 +48,5 @@ stdenv.mkDerivation (finalAttrs: {
       pSub
     ];
     platforms = lib.platforms.unix;
-    broken = stdenv.hostPlatform.isDarwin;
   };
 })
